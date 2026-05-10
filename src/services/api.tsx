59eb561d01f3597a906b27bd07ccfc1a7d871f15
Priscilla Brito <priscillabrito433@gmail.com>
2026-05-10 18:53:29 -0500

    fix api type error

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,14 +1,16 @@
-import type { Data } from "../interfaces/Movie";
+import type { Movie } from "../interfaces/Movie";
 
 const API_KEY = "22d8c17988d5590d8b2dc391f908e9ea";
 const BASE_URL = "https://api.themoviedb.org/3";
 
-export const getMovies = async (): Promise<Data> => {
-  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+export const getMovies = async (): Promise<Movie[]> => {
+  const response = await fetch(
+    `${BASE_URL}/movie/popular?api_key=${API_KEY}`
+  );
 
-  const data: Data = await response.json();
+  const data = await response.json();
 
   console.log(data.results);
 
-  return data;
+  return data.results;
 };
